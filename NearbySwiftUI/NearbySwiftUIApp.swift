af719ddd51e3e64e5dcc25eadcc883758b0048da
AlohaYos <yos@newtonjapan.com>
2023-03-08 08:04:04 +0900

    Initial Commit

---
+++ NearbySwiftUI/NearbySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearbySwiftUIApp.swift
+//  NearbySwiftUI
+//
+//  Created by Yos Hashimoto on 2023/03/08.
+//
+
+import SwiftUI
+
+@main
+struct NearbySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
